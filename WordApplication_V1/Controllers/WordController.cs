4566592d614e05b5ac1d90021097cb13d842551d
Ahmet Hakan Yıldız <teknomanihah@gmail.com>
2022-10-22 16:09:57 +0300

    Add project files.

---
+++ WordApplication_V1/Controllers/WordController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WordApplication_V1.Context;
+using WordApplication_V1.Entities;
+
+namespace WordApplication_V1.Controllers
+{
+    public class WordController : Controller
+    {
+        private readonly WordDbContext _dbContext ;
+
+   
+        public WordController()
+        {
+            _dbContext = new WordDbContext();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var list = _dbContext.Words.ToList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult AddWord()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddWord(Word word)
+        {
+            _dbContext.Words.Add(word);
+            _dbContext.SaveChanges();
+            return View(word);
+        }
+    }
+}
---
+++ WordApplication_V1/Entities/Word.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WordApplication_V1.Entities
+{
+    public class Word
+    {
+        
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="İngilizce alanı boş geçilemez.")]
+        public string EnName { get; set; }
+
+        [Required(ErrorMessage = "Türkçe karşılığı boş geçilemez.")]
+        public string TrName { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+    }
+}
